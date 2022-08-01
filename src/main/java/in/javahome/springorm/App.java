83c5e764db663d8a5920d447edd74f9832b321d5
Pruthvi360 <pruthvi99003871@gmail.com>
2022-08-01 19:02:20 +0530

    fisrt-commit

---
+++ src/main/java/in/javahome/springorm/App.java
@@ -0,0 +1,21 @@
+package in.javahome.springorm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.javahome.springorm.dao.EmployeeDAO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) throws Exception {
+		ApplicationContext cxt = new ClassPathXmlApplicationContext("spring-orm.xml");
+		EmployeeDAO employeeDAO = cxt.getBean(EmployeeDAO.class);
+//		Employee emp = new Employee();
+//		emp.setEmpName("Hari");
+//		emp.setPhone("999999999");
+		employeeDAO.deleteEmployee(5);
+	}
+}
